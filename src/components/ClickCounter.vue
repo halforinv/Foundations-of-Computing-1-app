5aa4333728bba35dd55b0630d2ae9466f3c1c323
tsukune3333 <invent.halhal15@gmail.com>
2024-07-24 20:13:36 +0900

    commit

---
+++ src/components/ClickCounter.vue
@@ -0,0 +1,4 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+</script>
+
---
+++ src/components/ScoreList.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+interface Item {
+  name: string
+  score: number
+}
+
+const scores = ref<Item[]>([
+])
+const newItemName = ref('')
+const newScore = ref(0)
+
+const addPerson = () => {
+  scores.value.push({ name: newItemName.value, score: newScore.value })
+}
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="person in scores" :key="person.name" :class="{ perfect: person.score == 100}">
+        <div>名前: {{ person.name }}</div>
+        <div>{{ person.score }} 点</div>
+        <div v-if="person.score < 60" :key="person.name" :class="{ bad: person.score << 60}">再履修</div>
+      </li>
+    </ul>
+    <div>
+      <label>
+        名前
+        <input v-model="newItemName" type="text" />
+      </label>
+      <label>
+        点数
+        <input v-model="newScore" type="number" />
+      </label>
+      <button @click="addPerson">追加</button>
+    </div>
+  </div>
+</template>
+
+<style>
+.perfect {
+  color: blue;
+}
+.bad {
+  color: red;
+}
+</style>
