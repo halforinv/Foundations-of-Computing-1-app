88dfe433809216b183d01d537124b6a8fdf47d97
halforinv <invent.halhal15@gmail.com>
2024-07-24 21:36:41 +0900

    commit3

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/{Foundations-of-Computing-1-app}/'
+  base: '/{halforinv/Foundations-of-Computing-1-app}/'
 })
